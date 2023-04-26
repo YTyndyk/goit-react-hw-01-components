0519bb7d46fa513cec0ee27bbf7416807fb4bded
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-04-26 18:14:08 +0300

    create profile and style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { Profile } from 'components/Profile/Profile';
+import css from 'components/Profile/Profile.module.css';
+import user from 'components/Profile/user.json';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.style}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
